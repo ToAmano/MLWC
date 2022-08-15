5f68fba8d88036873ed577e064b99835a08d1e34
dirac6582 <38810216+dirac6582@users.noreply.github.com>
2022-08-16 04:28:50 +0900

    finish show_traj ver1

--- cpmd/show_CP_tmp.py
+++ cpmd/show_CP_tmp.py
@@ -1,3 +1,5 @@
+filename="si_2/si_traj.xyz"
+JUMP="True"
 
 #
 # simple code to visualze XYZ by jupyter notebook  
@@ -6,12 +8,13 @@
 
 import ase.io
 import numpy as np
-test=ase.io.read(file, index=":")
+import nglview as nv
+test=ase.io.read(filename, index=":")
 
 # cp.xのoutputの場合，xyzの2行目が格子定数になっている．
 count_line=0
 # readlineで1行だけ読み込み
-with open(file) as f:
+with open(filename) as f:
     while True:
         count_line=count_line+1
         line = f.readline()
@@ -24,23 +27,25 @@ print(cell_vector) # 格子定数を取得
 for i in range(len(test)):
     test[i].set_cell(cell_vector) #Ang
 
-# 現状以下のjumpは正方晶にのみ有効
-# 格子の対角成分を取り出す．
-cell_check=cell_vector[np.arange(3),np.arange(3)]
-print(cell_check)
+#  ========================-
+
+if JUMP==False:
+    # 現状以下のjumpは正方晶にのみ有効
+    # 格子の対角成分を取り出す．
+    cell_check=cell_vector[np.arange(3),np.arange(3)]
+    # print(cell_check)
     
-# 
-import numpy as np
-for i in np.arange(1,len(test)):
-    coord=test[i].get_positions()
-    coord_before=test[i-1].get_positions()
-    # jumpする場合
-    if np.any(np.abs(coord_before-coord)>cell_check/2):
-        print("step :: ", i)
-        # jumpする場合には格子定数を追加する．
-        tmp1=np.where(coord-coord_before>cell_check/2 , coord, coord+cell_check)
-        tmp2=np.where(coord-coord_before<-cell_check/2, tmp1, tmp1-cell_check)
-        test[i].set_positions(tmp2)
+    # 
+    for i in np.arange(1,len(test)):
+        coord=test[i].get_positions()
+        coord_before=test[i-1].get_positions()
+        # jumpする場合
+        if np.any(np.abs(coord_before-coord)>cell_check/2):
+            # print("step :: ", i)
+            # jumpする場合には格子定数を追加する．
+            tmp1=np.where(coord-coord_before>cell_check/2 , coord, coord+cell_check)
+            tmp2=np.where(coord-coord_before<-cell_check/2, tmp1, tmp1-cell_check)
+            test[i].set_positions(tmp2)
 
 # print("fin nojump")
 # import numpy as np
@@ -50,22 +55,25 @@ for i in np.arange(1,len(test)):
 #     # jumpする場合
 #     if np.any(np.abs(coord_before-coord)>2.8):
 #         print("step :: ", i)
+#  ========================-
+
+
 
 # set pbc
 for i in np.arange(0,len(test)):
     test[i].pbc = (True, True, True)
 
-# save file in extended xyz
-ase.io.write(file+"_refine.xyz",images=test,format="xyz")
+# save filename in extended xyz
+ase.io.write(filename+"_refine.xyz",images=test,format="xyz")
 
 
     
 from ase.io.trajectory import Trajectory
-# save file in ase.traj
-ase.io.write(file+"_refine.traj",test,format="traj")
+# save filename in ase.traj
+ase.io.write(filename+"_refine.traj",test,format="traj")
 
 # reload as traj
-traj = Trajectory(file+"_refine.traj")
+traj = Trajectory(filename+"_refine.traj")
 view=nv.show_asetraj(test)
 view.parameters =dict(
                         camera_type="orthographic",
