edcdee375fa66c5939c3571592d3866f0410f5dd
dirac6582 <38810216+dirac6582@users.noreply.github.com>
2023-04-18 04:51:29 +0900

    [Refactor] 🐛 refactor ml_predict
    DO NOT use NUM_MOL in ml_predict.py

--- src/cmdline/ml_predict.py
+++ src/cmdline/ml_predict.py
@@ -366,11 +366,11 @@ def main():
         # 1! reshape(-1,3)としてしまう．
         
         # TODO : hard code (分子数)
-        NUM_MOL = 64
-        y_pred_ch = y_pred_ch.reshape((NUM_MOL*len(ch_bond_index),3))
-        y_pred_co = y_pred_co.reshape((NUM_MOL*len(co_bond_index),3))
-        y_pred_oh = y_pred_oh.reshape((NUM_MOL*len(oh_bond_index),3))
-        y_pred_o  = y_pred_o.reshape((NUM_MOL*len(o_index),3))
+        # NUM_MOL = 64
+        y_pred_ch = y_pred_ch.reshape((-1,3))
+        y_pred_co = y_pred_co.reshape((-1,3))
+        y_pred_oh = y_pred_oh.reshape((-1,3))
+        y_pred_o  = y_pred_o.reshape((-1,3))
 
         #予測したモデルを使ったUnit Cellの双極子モーメントの計算
         sum_dipole=np.sum(y_pred_ch,axis=0)+np.sum(y_pred_oh,axis=0)+np.sum(y_pred_co,axis=0)+np.sum(y_pred_o,axis=0)
