e193fea11f0d83cc389bd1539d50fd9d7934f86b
dirac6582 <38810216+dirac6582@users.noreply.github.com>
2023-05-21 15:10:46 +0900

    [Fix] bug fix cpml.py

--- src/cmdline/CPml.py
+++ src/cmdline/CPml.py
@@ -363,61 +363,61 @@ class NET(nn.Module):
 
 
 # * ここから予測させる，すなわちここからデータをロードして並列化
-def predict_dipole_mode1(fr,desc_dir):
-    #
-    # * 機械学習用のデータを読み込む
-    # *
-    #
-    global model_ch_2
-    global model_co_2
-    global model_oh_2
-    global model_o_2
+# def predict_dipole_mode1(fr,desc_dir):
+#     #
+#     # * 機械学習用のデータを読み込む
+#     # *
+#     #
+#     global model_ch_2
+#     global model_co_2
+#     global model_oh_2
+#     global model_o_2
 
-    # デバイスの設定    
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    nfeatures = 288
+#     # デバイスの設定    
+#     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+#     nfeatures = 288
 
-    # ring
-    # descs_X_ring = np.loadtxt('Descs_ring.csv',delimiter=',')
-    # data_y_ring = np.loadtxt('data_y_ring.csv',delimiter=',')
+#     # ring
+#     # descs_X_ring = np.loadtxt('Descs_ring.csv',delimiter=',')
+#     # data_y_ring = np.loadtxt('data_y_ring.csv',delimiter=',')
 
-    # CHボンド，COボンド，OHボンド，Oローンペア
-    descs_X_ch = np.loadtxt(desc_dir+'Descs_ch_'+str(fr)+'.csv',delimiter=',')
-    descs_X_co = np.loadtxt(desc_dir+'Descs_co_'+str(fr)+'.csv',delimiter=',')
-    descs_X_oh = np.loadtxt(desc_dir+'Descs_oh_'+str(fr)+'.csv',delimiter=',')
-    descs_X_o =  np.loadtxt(desc_dir+'Descs_o_'+str(fr)+'.csv',delimiter=',')
+#     # CHボンド，COボンド，OHボンド，Oローンペア
+#     descs_X_ch = np.loadtxt(desc_dir+'Descs_ch_'+str(fr)+'.csv',delimiter=',')
+#     descs_X_co = np.loadtxt(desc_dir+'Descs_co_'+str(fr)+'.csv',delimiter=',')
+#     descs_X_oh = np.loadtxt(desc_dir+'Descs_oh_'+str(fr)+'.csv',delimiter=',')
+#     descs_X_o =  np.loadtxt(desc_dir+'Descs_o_'+str(fr)+'.csv',delimiter=',')
 
-    # オリジナルの記述子を一旦tensorへ
-    X_ch = torch.from_numpy(descs_X_ch.astype(np.float32)).clone()
-    X_oh = torch.from_numpy(descs_X_oh.astype(np.float32)).clone()
-    X_co = torch.from_numpy(descs_X_co.astype(np.float32)).clone()
-    X_o  = torch.from_numpy(descs_X_o.astype(np.float32)).clone()
+#     # オリジナルの記述子を一旦tensorへ
+#     X_ch = torch.from_numpy(descs_X_ch.astype(np.float32)).clone()
+#     X_oh = torch.from_numpy(descs_X_oh.astype(np.float32)).clone()
+#     X_co = torch.from_numpy(descs_X_co.astype(np.float32)).clone()
+#     X_o  = torch.from_numpy(descs_X_o.astype(np.float32)).clone()
 
-    # 予測
-    y_pred_ch  = model_ch_2(X_ch.reshape(-1,nfeatures).to(device)).to("cpu").detach().numpy()
-    y_pred_co  = model_co_2(X_co.reshape(-1,nfeatures).to(device)).to("cpu").detach().numpy()
-    y_pred_oh  = model_oh_2(X_oh.reshape(-1,nfeatures).to(device)).to("cpu").detach().numpy()
-    y_pred_o   = model_o_2(X_o.reshape(-1,nfeatures).to(device)).to("cpu").detach().numpy()
+#     # 予測
+#     y_pred_ch  = model_ch_2(X_ch.reshape(-1,nfeatures).to(device)).to("cpu").detach().numpy()
+#     y_pred_co  = model_co_2(X_co.reshape(-1,nfeatures).to(device)).to("cpu").detach().numpy()
+#     y_pred_oh  = model_oh_2(X_oh.reshape(-1,nfeatures).to(device)).to("cpu").detach().numpy()
+#     y_pred_o   = model_o_2(X_o.reshape(-1,nfeatures).to(device)).to("cpu").detach().numpy()
 
-    # 最後にreshape
-    # !! ここは形としては(NUM_MOL*len(bond_index),3)となるが，予測だけする場合NUM_MOLの情報をgetできないので
-    # 1! reshape(-1,3)としてしまう．
+#     # 最後にreshape
+#     # !! ここは形としては(NUM_MOL*len(bond_index),3)となるが，予測だけする場合NUM_MOLの情報をgetできないので
+#     # 1! reshape(-1,3)としてしまう．
     
-    # TODO : hard code (分子数)
-    # NUM_MOL = 64
-    y_pred_ch = y_pred_ch.reshape((-1,3))
-    y_pred_co = y_pred_co.reshape((-1,3))
-    y_pred_oh = y_pred_oh.reshape((-1,3))
-    y_pred_o  = y_pred_o.reshape((-1,3))
-    print("DEBUG :: shape ch/co/oh/o :: {0} {1} {2} {3}".format(np.shape(y_pred_ch),np.shape(y_pred_co),np.shape(y_pred_oh),np.shape(y_pred_o)))
-    if fr == 0:
-        print("y_pred_ch ::", y_pred_ch)
-        print("y_pred_co ::", y_pred_co)
-        print("y_pred_oh ::", y_pred_oh)
-        print("y_pred_o  ::", y_pred_o)
-        #予測したモデルを使ったUnit Cellの双極子モーメントの計算
-    sum_dipole=np.sum(y_pred_ch,axis=0)+np.sum(y_pred_oh,axis=0)+np.sum(y_pred_co,axis=0)+np.sum(y_pred_o,axis=0)
-    return sum_dipole
+#     # TODO : hard code (分子数)
+#     # NUM_MOL = 64
+#     y_pred_ch = y_pred_ch.reshape((-1,3))
+#     y_pred_co = y_pred_co.reshape((-1,3))
+#     y_pred_oh = y_pred_oh.reshape((-1,3))
+#     y_pred_o  = y_pred_o.reshape((-1,3))
+#     print("DEBUG :: shape ch/co/oh/o :: {0} {1} {2} {3}".format(np.shape(y_pred_ch),np.shape(y_pred_co),np.shape(y_pred_oh),np.shape(y_pred_o)))
+#     if fr == 0:
+#         print("y_pred_ch ::", y_pred_ch)
+#         print("y_pred_co ::", y_pred_co)
+#         print("y_pred_oh ::", y_pred_oh)
+#         print("y_pred_o  ::", y_pred_o)
+#         #予測したモデルを使ったUnit Cellの双極子モーメントの計算
+#     sum_dipole=np.sum(y_pred_ch,axis=0)+np.sum(y_pred_oh,axis=0)+np.sum(y_pred_co,axis=0)+np.sum(y_pred_o,axis=0)
+#     return sum_dipole
 
 
 def calc_descripter_frame_and_predict_dipole(atoms_fr, fr, itp_data, NUM_MOL,NUM_MOL_ATOMS,UNITCELL_VECTORS,model_ch_2,model_oh_2,model_cc_2,model_co_2,model_o_2):
