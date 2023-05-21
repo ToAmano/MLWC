e0f23c05f66e52532f2a95835263c60b681b3097
dirac6582 <38810216+dirac6582@users.noreply.github.com>
2023-05-21 19:56:11 +0900

    [Add] debugging CPml_mpi.py

--- src/cmdline/CPml_mpi.py
+++ src/cmdline/CPml_mpi.py
@@ -44,6 +44,11 @@ from include.constants import constant
 # ang      = 1.0e-10
 coef    = constant.Ang*constant.Charge/constant.Debye
 
+def test(atoms_fr):
+    if np.all(atoms_fr == None):
+        return 0
+    return 0
+
 def calc_descripter_frame_descmode1(atoms_fr, fr, savedir, itp_data, NUM_MOL,NUM_MOL_ATOMS,UNITCELL_VECTORS ):
     '''
     記述子の保存：あり
@@ -52,47 +57,47 @@ def calc_descripter_frame_descmode1(atoms_fr, fr, savedir, itp_data, NUM_MOL,NUM
     '''
     if np.all(atoms_fr == None):
         return 0
-    # import cpmd.descripter
-    # import cpmd.asign_wcs
-    # # * wannierの割り当て部分のメソッド化
-    # ASIGN=cpmd.asign_wcs.asign_wcs(NUM_MOL,NUM_MOL_ATOMS,UNITCELL_VECTORS)
-    # DESC=cpmd.descripter.descripter(NUM_MOL,NUM_MOL_ATOMS,UNITCELL_VECTORS)
-    # # * 原子座標とボンドセンターの計算
-    # # 原子座標,ボンドセンターを分子基準で再計算
-    # results = ASIGN.aseatom_to_mol_coord_bc(atoms_fr, itp_data.bonds_list)
-    # list_mol_coords, list_bond_centers =results
-    # # * ボンドデータをさらにch/coなど種別ごとに分割 & 記述子を計算
-    # # mu_bondsの中身はchとringで分割する
-    # #mu_paiは全数をringにアサイン
-    # #mu_lpOとlpNはゼロ
-    # # ring
-    # if len(itp_data.ring_bond_index) != 0:
-    #     Descs_ring = []
-    #     ring_cent_mol = cpmd.descripter.find_specific_ringcenter(list_bond_centers, itp_data.ring_bond_index, 8, NUM_MOL)
-    #     i=0 
-    #     for bond_center in ring_cent_mol:
-    #         mol_id = i % NUM_MOL // 1
-    #         Descs_ring.append(DESC.get_desc_bondcent(atoms_fr,bond_center,mol_id))
-    #         i+=1 
+    import cpmd.descripter
+    import cpmd.asign_wcs
+    # * wannierの割り当て部分のメソッド化
+    ASIGN=cpmd.asign_wcs.asign_wcs(NUM_MOL,NUM_MOL_ATOMS,UNITCELL_VECTORS)
+    DESC=cpmd.descripter.descripter(NUM_MOL,NUM_MOL_ATOMS,UNITCELL_VECTORS)
+    # * 原子座標とボンドセンターの計算
+    # 原子座標,ボンドセンターを分子基準で再計算
+    results = ASIGN.aseatom_to_mol_coord_bc(atoms_fr, itp_data.bonds_list)
+    list_mol_coords, list_bond_centers =results
+    # * ボンドデータをさらにch/coなど種別ごとに分割 & 記述子を計算
+    # mu_bondsの中身はchとringで分割する
+    #mu_paiは全数をringにアサイン
+    #mu_lpOとlpNはゼロ
+    # ring
+    if len(itp_data.ring_bond_index) != 0:
+        Descs_ring = []
+        ring_cent_mol = cpmd.descripter.find_specific_ringcenter(list_bond_centers, itp_data.ring_bond_index, 8, NUM_MOL)
+        i=0 
+        for bond_center in ring_cent_mol:
+            mol_id = i % NUM_MOL // 1
+            Descs_ring.append(DESC.get_desc_bondcent(atoms_fr,bond_center,mol_id))
+            i+=1 
 
-    # # ch,oh,co,ccの計算と，その保存．
-    # # メモリ削減のため，ボンドごとに計算を行い，保存後速やかにメモリ解放
-    # Descs_ch=DESC.calc_bond_descripter_at_frame(atoms_fr,list_bond_centers,itp_data.ch_bond_index)
-    # # if len(itp_data.ch_bond_index) != 0: np.savetxt(savedir+'Descs_ch_'+str(fr)+'.csv', Descs_ch, delimiter=',')
-    # del Descs_ch
-    # Descs_oh=DESC.calc_bond_descripter_at_frame(atoms_fr,list_bond_centers,itp_data.oh_bond_index)
-    # # if len(itp_data.oh_bond_index) != 0: np.savetxt(savedir+'Descs_oh_'+str(fr)+'.csv', Descs_oh, delimiter=',') 
-    # del Descs_oh
-    # Descs_co=DESC.calc_bond_descripter_at_frame(atoms_fr,list_bond_centers,itp_data.co_bond_index)
-    # # if len(itp_data.co_bond_index) != 0: np.savetxt(savedir+'Descs_co_'+str(fr)+'.csv', Descs_co, delimiter=',')
-    # del Descs_co
-    # Descs_cc=DESC.calc_bond_descripter_at_frame(atoms_fr,list_bond_centers,itp_data.cc_bond_index)   
-    # # if len(itp_data.cc_bond_index) != 0: np.savetxt(savedir+'Descs_cc_'+str(fr)+'.csv', Descs_cc, delimiter=',')
-    # del Descs_cc
-    # # oローンペア
-    # Descs_o = DESC.calc_lonepair_descripter_at_frame(atoms_fr,list_mol_coords, itp_data.o_list, 8)
-    # # if len(itp_data.o_list) != 0: np.savetxt(savedir+'Descs_o_'+str(fr)+'.csv', Descs_o, delimiter=',')   
-    # del Descs_o
+    # ch,oh,co,ccの計算と，その保存．
+    # メモリ削減のため，ボンドごとに計算を行い，保存後速やかにメモリ解放
+    Descs_ch=DESC.calc_bond_descripter_at_frame(atoms_fr,list_bond_centers,itp_data.ch_bond_index)
+    # if len(itp_data.ch_bond_index) != 0: np.savetxt(savedir+'Descs_ch_'+str(fr)+'.csv', Descs_ch, delimiter=',')
+    del Descs_ch
+    Descs_oh=DESC.calc_bond_descripter_at_frame(atoms_fr,list_bond_centers,itp_data.oh_bond_index)
+    # if len(itp_data.oh_bond_index) != 0: np.savetxt(savedir+'Descs_oh_'+str(fr)+'.csv', Descs_oh, delimiter=',') 
+    del Descs_oh
+    Descs_co=DESC.calc_bond_descripter_at_frame(atoms_fr,list_bond_centers,itp_data.co_bond_index)
+    # if len(itp_data.co_bond_index) != 0: np.savetxt(savedir+'Descs_co_'+str(fr)+'.csv', Descs_co, delimiter=',')
+    del Descs_co
+    Descs_cc=DESC.calc_bond_descripter_at_frame(atoms_fr,list_bond_centers,itp_data.cc_bond_index)   
+    # if len(itp_data.cc_bond_index) != 0: np.savetxt(savedir+'Descs_cc_'+str(fr)+'.csv', Descs_cc, delimiter=',')
+    del Descs_cc
+    # oローンペア
+    Descs_o = DESC.calc_lonepair_descripter_at_frame(atoms_fr,list_mol_coords, itp_data.o_list, 8)
+    # if len(itp_data.o_list) != 0: np.savetxt(savedir+'Descs_o_'+str(fr)+'.csv', Descs_o, delimiter=',')   
+    del Descs_o
     # データが作成できているかの確認（debug）
     # print( " DESCRIPTOR SHAPE ")
     # print(" ring (Descs/data) ::", Descs_ring.shape)
@@ -740,7 +745,8 @@ def main():
                     print(" finish scattering data ...")
                     print("")
                 # frに変数が必要
-                result_dipole_tmp = calc_descripter_frame_descmode1(aseatom,fr,var_des.savedir,itp_data, NUM_MOL,NUM_MOL_ATOMS,UNITCELL_VECTORS)
+                # result_dipole_tmp = calc_descripter_frame_descmode1(aseatom,fr,var_des.savedir,itp_data, NUM_MOL,NUM_MOL_ATOMS,UNITCELL_VECTORS)
+                result_dipole_tmp = test(aseatom) # 
                 result_dipole_tmp = comm.gather(result_dipole_tmp, root=0) #どうもこのgatherがうまくいっていない．．．
                 if rank == 0:
                     print("")
