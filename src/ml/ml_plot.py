4de4b60b188d8b1b59af477eeed71e6525b2f17a
dirac6582 <38810216+dirac6582@users.noreply.github.com>
2023-09-16 10:34:10 +0900

    minor update src/ml

--- src/ml/ml_plot.py
+++ src/ml/ml_plot.py
@@ -1,8 +1,9 @@
 import torch
 import numpy as np
+import ml.ml_train
 
 def plot_loss_class(test,train):
-    
+    import matplotlib.pyplot as plt
     fig, ax = plt.subplots(figsize=(8,5),tight_layout=True) # figure, axesオブジェクトを作成
     scatter1=ax.scatter(np.arange(len(test)), test, label="test",alpha=0.2,color="#1f77b4", s=5)  # 描画
     scatter2=ax.scatter(np.arange(len(train)), train, label="train",alpha=0.2, color='#ff7f0e',s=5)
@@ -46,21 +47,196 @@ def plot_loss_class(test,train):
     # plt.show()
     return 0
 
-def plot_loss(test,train):
+def plot_loss(test_loss_list,train_loss_list,scale="normal")->int:
     '''
     学習ロスをプロットする関数
+    test :: testデータのloss値/RSME値
+    train :: trainデータのloss値/RSME値
     '''
     import matplotlib.pyplot as plt
     import numpy as np
-    test_x=np.arange(len(test))
-    plt.plot(test_x,test,label="test")
-    train_x=np.arange(len(train))
-    plt.plot(train_x,train,label="train")
+    test_x=np.arange(len(test_loss_list))
+    plt.plot(test_x,test_loss_list,label="test")
+    train_x=np.arange(len(train_loss_list))
+    plt.plot(train_x,train_loss_list,label="train")
+    if scale == "log":
+        plt.yscale("log")
     plt.legend()
     plt.title("loss of train/test")
     plt.xlabel("step")
     plt.ylabel("loss(%)")
     return 0
+
+def plot_loss_log(test_loss_list,train_loss_list):
+    '''
+    学習ロスをプロットする関数
+    '''
+    plot_loss(test_loss_list,train_loss_list,scale="log")
+    return 0
+
+
+def plot_residure_train_valid(model, dataset_train, dataset_valid,limit:bool=True):
+    '''
+    学習結果をplotする関数．
+    '''
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from ml.ml_train import calculate_final_dipoles
+    
+    # [size,3]の形でdipoleを返す
+    train_pred_list, train_true_list = calculate_final_dipoles(model, dataset_train)
+    valid_pred_list, valid_true_list = calculate_final_dipoles(model, dataset_valid)
+    
+    # RSMEを計算する
+    rmse_train = np.sqrt(np.mean((train_true_list-train_pred_list)**2))
+    rmse_valid = np.sqrt(np.mean((valid_true_list-valid_pred_list)**2))
+    print(" RSME_train = {0}".format(rmse_train))
+    print(" RSME_valid = {0}".format(rmse_valid))
+    
+    # matplotlibで複数のプロットをまとめる．
+    # https://python-academia.com/matplotlib-multiplegraphs/
+    # グラフを表示する領域を，figオブジェクトとして作成。
+    fig = plt.figure(figsize = (15,5), facecolor='lightblue')
+    
+    #グラフを描画するsubplot領域を作成。
+    ax1 = fig.add_subplot(1, 3, 1)
+    ax2 = fig.add_subplot(1, 3, 2)
+    ax3 = fig.add_subplot(1, 3, 3)
+    
+    #各subplot領域にデータを渡す
+    # TODO :: RSME，決定係数Rを同時に表示するようにする
+
+
+    ax1.scatter(train_pred_list[:,0], train_true_list[:,0], alpha=0.03, label="train")    
+    ax1.scatter(valid_pred_list[:,0], valid_true_list[:,0], alpha=0.03, label="valid")    
+    
+    ax2.scatter(train_pred_list[:,1], train_true_list[:,1], alpha=0.03, label="train")    
+    ax2.scatter(valid_pred_list[:,1], valid_true_list[:,1], alpha=0.03, label="valid")    
+    
+    ax3.scatter(train_pred_list[:,2], train_true_list[:,2], alpha=0.03, label="train")    
+    ax3.scatter(valid_pred_list[:,2], valid_true_list[:,2], alpha=0.03, label="valid")    
+
+    #タイトル
+    ax1.set_title("Dipole_x")
+    ax2.set_title("Dipole_y")
+    ax3.set_title("Dipole_z")
+
+    #各subplotにxラベルを追加
+    ax1.set_xlabel("ML dipole [D]")
+    ax2.set_xlabel("ML dipole [D]")
+    ax3.set_xlabel("ML dipole [D]")
+
+    ax1.set_ylabel("CPMD dipole [D]")
+    ax2.set_ylabel("CPMD dipole [D]")
+    ax3.set_ylabel("CPMD dipole [D]")
+
+    # 凡例表示
+    # https://qiita.com/hnii2006/items/2db5312fe4a4365734d0
+    legend1=ax1.legend(loc = 'upper left') #.get_frame().set_alpha(1.0)
+    legend2=ax2.legend(loc = 'upper left') # .get_frame().set_alpha(1.0) 
+    legend3=ax3.legend(loc = 'upper left') # .get_frame().set_alpha(1.0) 
+    
+    # grid表示
+    ax1.grid(True)
+    ax2.grid(True)
+    ax3.grid(True)
+
+    plt.show()
+
+    # if limit:
+    #     plt.xlim(-2,2)
+    #     plt.ylim(-2,2)
+    #    rmse_train = np.sqrt(np.mean((x0[:,i]-y0[:,i])**2))
+    #    rmse_test  = np.sqrt(np.mean((x1[:,i]-y1[:,i])**2))
+    #    print("rmse(train):  {0}  / rmse(test):  {1}".format(rmse_train,rmse_test))
+
+    return 0
+
+def calculate_gaussian_kde(data_x,data_y):
+    # https://runtascience.hatenablog.com/entry/2021/05/06/%E3%80%90Matplotlib%E3%80%91python%E3%81%A7%E5%AF%86%E5%BA%A6%E3%83%97%E3%83%AD%E3%83%83%E3%83%88%28Density_plot%29
+    from scipy.stats import gaussian_kde
+    # KDE probability
+    x = data_x
+    y = data_y
+    xy = np.vstack([x,y])
+    z = gaussian_kde(xy)(xy)
+    # zの値で並び替え→x,yも並び替える
+    idx = z.argsort() 
+    x, y, z = x[idx], y[idx], z[idx]
+    return x,y,z
+
+def plot_residure_density(model, dataset,limit:bool=True):
+    '''
+    学習結果をplotする関数．
+    こちらではtrain/validの区別なく，全てのデータをまとめて，代わりにdensity mapで表示する
+    '''
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from ml.ml_train import calculate_final_dipoles
+    
+    # [size,3]の形でdipoleを返す
+    pred_list, true_list = calculate_final_dipoles(model, dataset)
+    
+    # RSMEを計算する
+    rmse = np.sqrt(np.mean((true_list-pred_list)**2))
+    print(" RSME_train = {0}".format(rmse))
+    
+    # matplotlibで複数のプロットをまとめる．
+    # https://python-academia.com/matplotlib-multiplegraphs/
+    # グラフを表示する領域を，figオブジェクトとして作成。
+    fig = plt.figure(figsize = (15,5), facecolor='lightblue')
+    
+    #グラフを描画するsubplot領域を作成。
+    ax1 = fig.add_subplot(1, 3, 1)
+    ax2 = fig.add_subplot(1, 3, 2)
+    ax3 = fig.add_subplot(1, 3, 3)
+    
+    #各subplot領域にデータを渡す
+    # TODO :: RSME，決定係数Rを同時に表示するようにする
+
+    # KDE probability
+    x,y,z = calculate_gaussian_kde(pred_list[:,0], true_list[:,0])
+    im = ax1.scatter(x, y, c=z, s=50, cmap="jet")
+    fig.colorbar(im)
+
+    x,y,z = calculate_gaussian_kde(pred_list[:,1], true_list[:,1])
+    im = ax2.scatter(x, y, c=z, s=50, cmap="jet")
+    fig.colorbar(im)
+
+    x,y,z = calculate_gaussian_kde(pred_list[:,2], true_list[:,2])
+    im = ax3.scatter(x, y, c=z, s=50, cmap="jet")
+    fig.colorbar(im)
+
+    #各subplotにxラベルを追加
+    ax1.set_xlabel("ML dipole [D]")
+    ax2.set_xlabel("ML dipole [D]")
+    ax3.set_xlabel("ML dipole [D]")
+
+    ax1.set_ylabel("CPMD dipole [D]")
+    ax2.set_ylabel("CPMD dipole [D]")
+    ax3.set_ylabel("CPMD dipole [D]")
+
+    # 凡例表示
+    ax1.legend(loc = 'upper left') 
+    ax2.legend(loc = 'upper left') 
+    ax3.legend(loc = 'upper left') 
+
+    # grid表示
+    ax1.grid(True)
+    ax2.grid(True)
+    ax3.grid(True)
+
+    plt.show()
+
+    # if limit:
+    #     plt.xlim(-2,2)
+    #     plt.ylim(-2,2)
+    #    rmse_train = np.sqrt(np.mean((x0[:,i]-y0[:,i])**2))
+    #    rmse_test  = np.sqrt(np.mean((x1[:,i]-y1[:,i])**2))
+    #    print("rmse(train):  {0}  / rmse(test):  {1}".format(rmse_train,rmse_test))
+
+    return 0
+
 
 def plot_residure(y_pred_train,y_pred_test,true_y,test_y,limit:bool=True):
     '''
