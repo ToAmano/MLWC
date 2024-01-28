d564318a66022960a65163cf15e54b996bd7c31b
dirac6582 <38810216+dirac6582@users.noreply.github.com>
2024-01-28 13:35:52 +0900

    add save_dipole in postprocess

---
+++ notebook/c++/src/postprocess/save_dipole.cpp
@@ -0,0 +1,74 @@
+// https://github.com/microsoft/vscode-cpptools/issues/7413
+#if __INTELLISENSE__
+#undef __ARM_NEON
+#undef __ARM_NEON__
+#endif
+
+/**
+ * @file save_dipole.cpp
+ * @author Tomohito Amano (you@domain.com)
+ * @brief wrapper of savevec.cpp
+ * @version 0.1
+ * @date 2024-01-28
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+// #define _DEBUG
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream> // https://www.cns.s.u-tokyo.ac.jp/~masuoka/post/inputfile_cpp/
+#include <algorithm>
+#include <numeric> // std::iota
+// #include <boost/numeric/ublas/vector.hpp>
+// #include <boost/numeric/ublas/matrix.hpp>
+// #include <boost/numeric/ublas/io.hpp>
+#include <Eigen/Core> // 行列演算など基本的な機能．
+
+// https://qiita.com/meshidenn/items/53b7c6f35c6259320241
+#include "../include/savevec.hpp"
+
+
+int save_totaldipole(const std::vector<Eigen::Vector3d>& result_dipole_list, std::vector<std::vector<double> > unitcell, double temperature, double timestep, std::string savedir){
+
+    // 最後にtotal双極子をファイルに保存
+    std::stringstream ss;
+    ss << "# index dipole_x dipole_y dipole_z \n"
+        << "#UNITCELL[Ang] " 
+        << unitcell[0][0] << " " << unitcell[0][1] << " " << unitcell[0][2] << " " << unitcell[1][0] << " " << unitcell[1][1] << " " << unitcell[1][2] << " " << unitcell[2][0] << " " << unitcell[2][1] << " " << unitcell[2][2]
+        << "\n" 
+        << "#TEMPERATURE[K] "
+        << temperature
+        << "\n"
+        << "#TIMESTEP[fs]"
+        << timestep;
+    std::string firstline_tmp = ss.str();
+    save_vec(result_dipole_list, savedir+"total_dipole.txt", firstline_tmp);
+    return 0;
+}
+
+
+int save_bonddipole(const std::vector<std::vector<Eigen::Vector3d> >& result_bond_dipole_list,std::string savedir,std::string filename){
+    // save files1: bond dipoleをファイルに保存
+    // TODO :: （3D配列なのでもっと良い方法を考えないといけない） 
+    std::string firstline = "# frame_index bond_index dipole_x dipole_y dipole_z";
+    save_vec_index(result_bond_dipole_list,savedir+"/"+filename, firstline);
+    return 0;
+}
+
+int save_moleculedipole(const std::vector<std::vector<Eigen::Vector3d> >& result_mol_dipole_list,std::string savedir){
+    // 分子双極子の保存：本来3次元配列だが，frame,mol_id,d_x,d_y,d_zの形で保存することで二次元配列として保存する．
+    std::string firstline = "# frame_index mol_index dipole_x dipole_y dipole_z";
+    save_vec_index(result_mol_dipole_list, savedir+"/molecule_dipole.txt", firstline);
+    return 0;
+}
+
