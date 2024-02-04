65dca7bf594609280dc8433d2bdd5aec1ff68abb
dirac6582 <38810216+dirac6582@users.noreply.github.com>
2024-02-04 20:12:59 +0900

    test add yaml

--- notebook/c++/src/module_input.cpp
+++ notebook/c++/src/module_input.cpp
@@ -22,6 +22,7 @@
 #include "include/manupilate_files.hpp"
 #include "module_input.hpp"
 #include "parse.hpp"
+#include "yaml-cpp/yaml.h" 
 
 namespace module_input{
 load_input::load_input(std::string xyzfilename, std::unique_ptr<diagnostics::Stopwatch> &timer){
@@ -37,7 +38,11 @@ load_input::load_input(std::string xyzfilename, std::unique_ptr<diagnostics::Sto
     //
     timer->reset(); // reset timer
     timer->start(); // restart timer
-    load_input::_get_input_text(); // load ALL_NUM_ATOM (include wannier)
+    if (this->_inputfilename.ends_with("yaml")){
+        load_input::_get_input_yaml(); // load yaml
+    } else{
+        load_input::_get_input_text(); // load text
+    }
     load_input::_check_savedir();
     timer->stop(); // stop timer
     std::cout << "     ELAPSED TIME (sec)      = " << timer->getElapsedSeconds() << std::endl;
@@ -45,11 +50,11 @@ load_input::load_input(std::string xyzfilename, std::unique_ptr<diagnostics::Sto
 };
 
 int load_input::_get_input_text(){
-    auto [inp_general, inp_desc, inp_pred] = locate_tag(this->_inputfilename);
+    auto [inp_general, inp_desc, inp_pred] = parse::locate_tag(this->_inputfilename);
     std::cout << "FINISH reading inp file !! " << std::endl;
-    auto tmp_var_gen = var_general(inp_general);
-    auto tmp_var_des = var_descripter(inp_desc);
-    auto tmp_var_pre = var_predict(inp_pred);
+    auto tmp_var_gen = parse::var_general(inp_general);
+    auto tmp_var_des = parse::var_descripter(inp_desc);
+    auto tmp_var_pre = parse::var_predict(inp_pred);
     this->var_gen = tmp_var_gen;
     this->var_des = tmp_var_des;
     this->var_pre = tmp_var_pre;
@@ -63,6 +68,34 @@ int load_input::_get_input_text(){
     }
     return 0;
 };
+
+int load_input::_get_input_yaml(){
+
+    YAML::Node node   = YAML::LoadFile(this->_inputfilename);
+    YAML::Node config_gen = node["general"];
+    YAML::Node config_pre = node["predict"];
+    YAML::Node config_des = node["descriptor"];
+    if (config_gen.size() == 0){error::exit("_get_input_yaml", "ERROR NO &general fuund");};
+    if (config_des.size() == 0){error::exit("_get_input_yaml", "ERROR NO &descriptor fuund");};
+    if (config_pre.size() == 0){error::exit("_get_input_yaml", "ERROR NO &predict fuund");};
+    std::cout << "FINISH reading inp file !! " << std::endl;
+    auto tmp_var_gen = parse::var_general(config_gen);
+    auto tmp_var_des = parse::var_descripter(config_des);
+    auto tmp_var_pre = parse::var_predict(config_pre);
+    this->var_gen = tmp_var_gen;
+    this->var_des = tmp_var_des;
+    this->var_pre = tmp_var_pre;
+
+    std::cout << "FINISH parse inp file !! " << std::endl;
+    //
+    if (var_des.IF_COC){
+        std::cout << " =============== " << std::endl;
+        std::cout << "  IF_COC is true " << std::endl;
+        std::cout << " =============== " << std::endl;
+    }
+    return 0;
+};
+
 
 int load_input::_check_savedir(){
     //! 保存するディレクトリの存在を確認
--- notebook/c++/src/parse.hpp
+++ notebook/c++/src/parse.hpp
@@ -8,8 +8,10 @@
 #include <string>
 #include <cctype> // https://b.0218.jp/20150625194056.html
 #include <algorithm> 
+#include "yaml-cpp/yaml.h" //https://github.com/jbeder/yaml-cpp
+#include "include/error.h"
 
-
+namespace parse{
 // 任意のdilimiterで分割する関数
 // https://maku77.github.io/cpp/string/split.html
 //!! default variable should be only in header file
@@ -19,6 +21,20 @@ std::string remove_space(const std::string& str);
 
 std::tuple<std::vector<std::vector<std::string > >, std::vector<std::vector<std::string > >, std::vector<std::vector<std::string > > > locate_tag(std::string inputfilename );
 
+// >>>> FOR YAML >>>>>>
+int get_len_yaml(YAML::Node node); //YAML::Node node   = YAML::LoadFile("config.yaml");
+
+std::string get_val_yaml(YAML::Node node, std::string key); // keyに対応するvalを取得(string)
+
+std::string parse_required_argment(YAML::Node node, std::string key);
+
+std::string parse_optional_argment(YAML::Node node, std::string key, std::string default_val);
+
+bool if_val_exist(YAML::Node node, std::string key);
+// >>>> END FOR YAML >>>>>>
+
+
+
 class var_general{
     /*
     general用の変数を一括管理する
@@ -26,12 +42,13 @@ class var_general{
     */
     public:
         std::string itpfilename; // itpファイルのファイル名
-        std::string bondfilename; // bondファイルの名前
-        std::string savedir; // 記述子の保存dir
-        double temperature = 300; // 温度[ケルビン] (default 300K)
-        double timestep    = 0.5; // 時間刻み[fs] ( default 0.5fs)
+        std::string bondfilename; // necessary:: bondファイルの名前
+        std::string savedir; // necessary:: 記述子の保存dir
+        double temperature = 300; // Temperature [K] (default 300K)
+        double timestep    = 0.5; // MD timestep [fs] ( default 0.5fs)
         var_general();
         var_general(std::vector< std::vector<std::string> > input_general);
+        var_general(YAML::Node node); // if input is yaml
 };
 
 
@@ -51,10 +68,11 @@ class var_descripter{
     int haswannier = 0; // 1がTrue，0がFalse (デフォルトが0, nonwanで有効)
     int interval = 1; // trajectoryを何ステップごとに処理するか．デフォルトは毎ステップ．(optional)
     std::string desctype = "old"; // 記述子の種類 old or allinone
-    int IF_COC = 0; // 1がTrue，0がFalse (デフォルトが0, COC記述子を有効化する．)
+    int IF_COC = 0; // 1がTrue，0がFalse (デフォルトが0, COC記述子を有効化する．) //!! 未使用
     int IF_GAS = 0; // 1がTrue， gasモデル計算を有効にする場合
     var_descripter();
     var_descripter(std::vector< std::vector<std::string> > input_descripter);
+    var_descripter(YAML::Node node);
 };
 
 
@@ -65,11 +83,14 @@ class var_predict{
 
    public:
     int calc; // 計算するかどうかのフラグ（1がTrue，0がFalse）
-    std::string model_dir; // modelのディレクトリ
+    std::string model_dir; // modelのディレクトリ(必須)
     std::string desc_dir; // 記述子のロードdir
-    std::string modelmode; // normal or rotate (2023/4/16)
-    int bondspecies = 4 ; // デフォルトの4はメタノールに対応
+    std::string modelmode; // normal or rotate (2023/4/16) // 2024/2/4 :: C++版には不要
+    int bondspecies = 4 ; // デフォルトの4はメタノールに対応 
     int save_truey = 0 ; // 1がTrue，0がFalse（true_yを保存するかどうか．）
     var_predict();
     var_predict(std::vector< std::vector<std::string> > input_predict);
+    var_predict(YAML::Node node);
 };
+
+} //END namespace
