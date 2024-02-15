2e8ee7686f84cbc851a89a473d9ce7a3bf38c755
dirac6582 <38810216+dirac6582@users.noreply.github.com>
2024-02-15 17:35:18 +0900

    save unitcell to bond_dipole.txt

--- notebook/c++/src/postprocess/save_dipole.cpp
+++ notebook/c++/src/postprocess/save_dipole.cpp
@@ -37,6 +37,19 @@
 // https://qiita.com/meshidenn/items/53b7c6f35c6259320241
 #include "../include/savevec.hpp"
 
+std::string get_preface_line(std::vector<std::vector<double> > unitcell, double temperature, double timestep){
+    std::stringstream ss;
+    ss << "#UNITCELL[Ang] " 
+        << unitcell[0][0] << " " << unitcell[0][1] << " " << unitcell[0][2] << " " << unitcell[1][0] << " " << unitcell[1][1] << " " << unitcell[1][2] << " " << unitcell[2][0] << " " << unitcell[2][1] << " " << unitcell[2][2]
+        << "\n" 
+        << "#TEMPERATURE[K] " // spaceが必須
+        << temperature
+        << "\n"
+        << "#TIMESTEP[fs] "
+        << timestep;
+    std::string firstline_tmp = ss.str();
+    return firstline_tmp;
+}
 
 int save_totaldipole(const std::vector<Eigen::Vector3d>& result_dipole_list, std::vector<std::vector<double> > unitcell, double temperature, double timestep, std::string savedir){
 
@@ -49,7 +62,7 @@ int save_totaldipole(const std::vector<Eigen::Vector3d>& result_dipole_list, std
         << "#TEMPERATURE[K] "
         << temperature
         << "\n"
-        << "#TIMESTEP[fs]"
+        << "#TIMESTEP[fs] "
         << timestep;
     std::string firstline_tmp = ss.str();
     save_vec(result_dipole_list, savedir+"total_dipole.txt", firstline_tmp);
@@ -81,21 +94,23 @@ int postprocess_save_bonddipole(
     const std::vector<std::vector<Eigen::Vector3d> >& result_coc_dipole_list,
     const std::vector<std::vector<Eigen::Vector3d> >& result_coh_dipole_list,
     const std::vector<std::vector<Eigen::Vector3d> >& result_molecule_dipole_list,
+    std::vector<std::vector<double> > unitcell, double temperature, double timestep,
     std::string savedir){
 
-    std::string firstline = "# frame_index bond_index dipole_x dipole_y dipole_z";
+    std::string firstline = "# frame_index bond_index dipole_x dipole_y dipole_z \n";
+    std::string preface   = get_preface_line(unitcell, temperature,timestep);
 
     // TODO :: （3D配列なのでもっと良い方法を考えないといけない） 
-    save_vec_index(result_ch_dipole_list,savedir+"/ch_dipole.txt", "# chbond dipole \n" + firstline);
-    save_vec_index(result_co_dipole_list,savedir+"/co_dipole.txt", "# cobond dipole \n" + firstline);
-    save_vec_index(result_oh_dipole_list,savedir+"/oh_dipole.txt", "# ohbond dipole \n" + firstline);
-    save_vec_index(result_cc_dipole_list,savedir+"/cc_dipole.txt", "# ccbond dipole \n" + firstline);
-    save_vec_index(result_o_dipole_list ,savedir+ "/o_dipole.txt", "# obond dipole \n" + firstline);
-    save_vec_index(result_coc_dipole_list,savedir+ "/coc_dipole.txt", "# cocbond dipole \n" + firstline);
-    save_vec_index(result_coh_dipole_list,savedir+ "/coh_dipole.txt", "# cohbond dipole \n" + firstline);
+    save_vec_index(result_ch_dipole_list,savedir+"/ch_dipole.txt", "# chbond dipole \n" + firstline + preface);
+    save_vec_index(result_co_dipole_list,savedir+"/co_dipole.txt", "# cobond dipole \n" + firstline + preface);
+    save_vec_index(result_oh_dipole_list,savedir+"/oh_dipole.txt", "# ohbond dipole \n" + firstline + preface);
+    save_vec_index(result_cc_dipole_list,savedir+"/cc_dipole.txt", "# ccbond dipole \n" + firstline + preface);
+    save_vec_index(result_o_dipole_list ,savedir+ "/o_dipole.txt", "# obond dipole \n" + firstline + preface);
+    save_vec_index(result_coc_dipole_list,savedir+ "/coc_dipole.txt", "# cocbond dipole \n" + firstline + preface);
+    save_vec_index(result_coh_dipole_list,savedir+ "/coh_dipole.txt", "# cohbond dipole \n" + firstline + preface);
 
     // 分子双極子の保存：本来3次元配列だが，frame,mol_id,d_x,d_y,d_zの形で保存することで二次元配列として保存する．
-    save_vec_index(result_molecule_dipole_list, savedir+"/molecule_dipole.txt", "# molecular dipole \n" + firstline);
+    save_vec_index(result_molecule_dipole_list, savedir+"/molecule_dipole.txt", "# molecular dipole \n" + firstline + preface);
 
     return 0;
 
