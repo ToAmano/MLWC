0e073037ec930bc5a186a7b524cda7887419055c
dirac6582 <38810216+dirac6582@users.noreply.github.com>
2024-03-24 16:48:47 +0900

    bug fix ml_loss

--- src/ml/ml_loss.py
+++ src/ml/ml_loss.py
@@ -33,7 +33,7 @@ class LossStatistics:
         with open("train_batch_loss.txt", 'w') as f:
             print(f"{iepoch}  {len(self.df_batch_train)} {loss} {np.sqrt(loss)}", file=f)  # 引数はstr関数と同様に文字列化される
 
-        # TODO :: もしepochが更新されたら，epoch_lossを更新
+        # if new epoch, print epoch result
         if iepoch > self.df_batch_train[-1]["epoch"]:
             self.add_train_epoch_loss()
             self.reset_train_batch_loss()
@@ -49,8 +49,8 @@ class LossStatistics:
         import numpy as np
         with open("valid_batch_loss.txt", 'w') as f:
             print(f"{iepoch}  {len(self.df_batch_valid)} {loss} {np.sqrt(loss)}", file=f)  # 引数はstr関数と同様に文字列化される
-
-        # TODO :: もしepochが更新されたら，epoch_lossを更新
+            
+        # if new epoch, print epoch result
         if iepoch > self.df_batch_valid[-1]["epoch"]:
             self.add_valid_epoch_loss()
             self.reset_valid_batch_loss()
@@ -92,121 +92,60 @@ class LossStatistics:
     def save_train_batch_loss():
         return 0        
 
-
+    def print_current_result():
+        return 0
 
     
     
-    def __init__(self, loss_instance=None):
-        self.loss_stat = {
-            "total": RunningStats(
-                dim=tuple(), reduction=Reduction.MEAN, ignore_nan=False
-            )
-        }
-        self.ignore_nan = {}
-        if loss_instance is not None:
-            for key, func in loss_instance.funcs.items():
-                self.ignore_nan[key] = (
-                    func.ignore_nan if hasattr(func, "ignore_nan") else False
-                )
-
-    def __call__(self, loss, loss_contrib):
-        """
-        Args:
-
-        loss (torch.Tensor): the value of the total loss function for the current batch
-        loss (Dict(torch.Tensor)): the dictionary which contain the loss components
-        """
-
-        results = {}
-
-        results["loss"] = self.loss_stat["total"].accumulate_batch(loss).item()
-
-        # go through each component
-        for k, v in loss_contrib.items():
-
-            # initialize for the 1st batch
-            if k not in self.loss_stat:
-                self.loss_stat[k] = RunningStats(
-                    dim=tuple(),
-                    reduction=Reduction.MEAN,
-                    ignore_nan=self.ignore_nan.get(k, False),
-                )
-                device = v.get_device()
-                self.loss_stat[k].to(device="cpu" if device == -1 else device)
-
-            results["loss_" + ABBREV.get(k, k)] = (
-                self.loss_stat[k].accumulate_batch(v).item()
-            )
-        return results
-
-    def reset(self):
-        """
-        Reset all the counters to zero
-        """
-
-        for v in self.loss_stat.values():
-            v.reset()
-
-    def to(self, device):
-        for v in self.loss_stat.values():
-            v.to(device=device)
-
-    def current_result(self):
-        results = {
-            "loss_" + ABBREV.get(k, k): v.current_result().item()
-            for k, v in self.loss_stat.items()
-            if k != "total"
-        }
-        results["loss"] = self.loss_stat["total"].current_result().item()
-        return results
 
 
 
-    def end_of_batch_log(self, batch_type: str):
-        """
-        store all the loss/mae of each batch
-        """
 
-        mat_str = f"{self.iepoch+1:5d}, {self.ibatch+1:5d}"
-        log_str = f"  {self.iepoch+1:5d} {self.ibatch+1:5d}"
+    # def end_of_batch_log(self, batch_type: str):
+    #     """
+    #     store all the loss/mae of each batch
+    #     """
 
-        header = "epoch, batch"
-        log_header = "# Epoch batch"
+    #     mat_str = f"{self.iepoch+1:5d}, {self.ibatch+1:5d}"
+    #     log_str = f"  {self.iepoch+1:5d} {self.ibatch+1:5d}"
 
-        # print and store loss value
-        for name, value in self.batch_losses.items():
-            mat_str += f", {value:16.5g}"
-            header += f", {name}"
-            log_str += f" {value:12.3g}"
-            log_header += f" {name:>12.12}"
+    #     header = "epoch, batch"
+    #     log_header = "# Epoch batch"
 
-        # append details from metrics
-        metrics, skip_keys = self.metrics.flatten_metrics(
-            metrics=self.batch_metrics,
-            type_names=self.dataset_train.type_mapper.type_names,
-        )
-        for key, value in metrics.items():
+    #     # print and store loss value
+    #     for name, value in self.batch_losses.items():
+    #         mat_str += f", {value:16.5g}"
+    #         header += f", {name}"
+    #         log_str += f" {value:12.3g}"
+    #         log_header += f" {name:>12.12}"
 
-            mat_str += f", {value:16.5g}"
-            header += f", {key}"
-            if key not in skip_keys:
-                log_str += f" {value:12.3g}"
-                log_header += f" {key:>12.12}"
+    #     # append details from metrics
+    #     metrics, skip_keys = self.metrics.flatten_metrics(
+    #         metrics=self.batch_metrics,
+    #         type_names=self.dataset_train.type_mapper.type_names,
+    #     )
+    #     for key, value in metrics.items():
 
-        batch_logger = logging.getLogger(self.batch_log[batch_type])
+    #         mat_str += f", {value:16.5g}"
+    #         header += f", {key}"
+    #         if key not in skip_keys:
+    #             log_str += f" {value:12.3g}"
+    #             log_header += f" {key:>12.12}"
 
-        if self.ibatch == 0:
-            self.logger.info("")
-            self.logger.info(f"{batch_type}")
-            self.logger.info(log_header)
-            init_step = -1 if self.report_init_validation else 0
-            if (self.iepoch == init_step and batch_type == VALIDATION) or (
-                self.iepoch == 0 and batch_type == TRAIN
-            ):
-                batch_logger.info(header)
+    #     batch_logger = logging.getLogger(self.batch_log[batch_type])
 
-        batch_logger.info(mat_str)
-        if (self.ibatch + 1) % self.log_batch_freq == 0 or (
-            self.ibatch + 1
-        ) == self.n_batches:
-            self.logger.info(log_str)
+    #     if self.ibatch == 0:
+    #         self.logger.info("")
+    #         self.logger.info(f"{batch_type}")
+    #         self.logger.info(log_header)
+    #         init_step = -1 if self.report_init_validation else 0
+    #         if (self.iepoch == init_step and batch_type == VALIDATION) or (
+    #             self.iepoch == 0 and batch_type == TRAIN
+    #         ):
+    #             batch_logger.info(header)
+
+    #     batch_logger.info(mat_str)
+    #     if (self.ibatch + 1) % self.log_batch_freq == 0 or (
+    #         self.ibatch + 1
+    #     ) == self.n_batches:
+    #         self.logger.info(log_str)
