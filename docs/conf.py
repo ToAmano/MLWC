8f029ca2001c3b2e9004377f5fdfba3eff171112
dirac6582 <38810216+dirac6582@users.noreply.github.com>
2024-07-15 00:21:03 +0900

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath('../'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'dieltools'
+project = 'MLWC'
 copyright = '2024, Tomohito Amano, Tamio Yamazaki'
 author = 'Tomohito Amano, Tamio Yamazaki'
 
