e34039e77ae23f7e13ec1177411a5f7761475dd0
dirac6582 <38810216+dirac6582@users.noreply.github.com>
2024-08-18 20:56:09 +0900

    bug fix cpextract_diel

--- src/cmdline/cpextract_diel.py
+++ src/cmdline/cpextract_diel.py
@@ -490,7 +490,7 @@ def command_diel_spectra(args):
     EVP=Plot_totaldipole(args.Filename)
     # moving average:: https://chaos-kiyono.hatenablog.com/entry/2022/07/25/212843
     # https://qiita.com/FallnJumper/items/e0afa1fb05ea448caae1
-    EVP.calc_dielectric_spectrum(float(args.eps),int(args.start),int(args.end),int(args.step),int(args.window)) # epsを受け取ってfloat変換
+    EVP.calc_dielectric_spectrum(float(args.eps),int(args.start),int(args.end),int(args.step),args.window) # epsを受け取ってfloat変換
     EVP.calc_dielectric_derivative_spectrum(int(args.start), int(args.end), int(args.step)) # 微分公式のテスト
     return 0
 
