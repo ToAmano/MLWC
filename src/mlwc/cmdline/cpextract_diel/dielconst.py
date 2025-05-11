3e27d91a01e7452f06aa97291df4bc0680b3ba88
dirac6582 <38810216+dirac6582@users.noreply.github.com>
2025-05-12 05:19:47 +0900

    🚧 formatter for dielconst/gfactor

--- src/mlwc/cmdline/cpextract_diel/dielconst.py
+++ src/mlwc/cmdline/cpextract_diel/dielconst.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from mlwc.fourier.totaldipole.totaldipole import TotalDipole
 from mlwc.fourier.totaldipole.totaldipole_io import read_file
 from mlwc.include.mlwc_logger import setup_library_logger
@@ -6,9 +8,12 @@ logger = setup_library_logger("MLWC." + __name__)
 
 
 def command_diel_dielconst(args):
+    """Calculate dielectric constant and check convergence"""
     logger.info(" cpextract.py diel dielconst")
     totaldipole_instance: TotalDipole = read_file(args.Filename)
     totaldipole_instance.print_info()
-    df = totaldipole_instance.calc_time_vs_dielconst(int(args.start), int(args.end))
+    df: pd.DataFrame = totaldipole_instance.calc_time_vs_dielconst(
+        int(args.start), int(args.end)
+    )
     totaldipole_instance.plot_time_vs_dielconst(df)
     return 0
